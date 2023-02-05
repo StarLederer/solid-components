97a23ac294cb2b0a0822b62fb1dcc6c390152e7e
Herman Lederer <germans.lederers@gmail.com>
2023-02-05 23:19:40 +0100

    Remove hardcode from Route

--- router/Route.tsx
+++ router/Route.tsx
@@ -1,68 +1,79 @@
-import { createEffect, createSignal, ParentComponent, Show } from "solid-js";
+import { createEffect, createSignal, on, ParentComponent, Show } from "solid-js";
 import router from "./index";
 
-type IRouteProps = {
+export type RouteProps = {
   path: string;
   strict?: boolean;
-  scroll?: boolean;
   onOpen?: () => void;
   onClose?: () => void;
+  enter?: string;
+  leave?: string;
+  class?: string;
 };
 
-const Route: ParentComponent<IRouteProps> = (props) => {
+const Route: ParentComponent<RouteProps> = (props) => {
   const { route } = router;
-  const baseClasses = "absolute inset-0";
-  const classes = () => (
-    baseClasses
-    + (props.scroll ? " overflow-auto" : "")
-    + (routeIn() ? " animate-in" : "")
-    + (routeOut() ? " animate-out" : "")
-  );
 
-  // Transitions
+  // Determine whether this path is open.
 
-  const pathLast = () => props.path.split("/").pop();
-
-  const routeIn = () => route().firstDifferent[1] === pathLast();
-  const routeOut = () => route().firstDifferent[0] === pathLast();
-
-  // State
-
-  let timeout = setTimeout(() => { }, 0);
-
-  const isOpen = () => {
-    let open = false;
-
-    if (props.strict) {
-      open = route().current === props.path;
-    } else {
-      open = route().current.startsWith(props.path);
-    }
-
-    if (open) props.onOpen?.();
-    else props.onClose?.();
-
-    return open;
-  };
-
-  const [hasDom, setHasDom] = createSignal(false);
+  const [isOpen, setIsOpen] = createSignal(false);
 
   createEffect(() => {
-    if (isOpen()) {
-      setHasDom(true);
-      clearTimeout(timeout);
+    if (props.strict) {
+      setIsOpen(route().current === props.path);
     } else {
-      timeout = setTimeout(() => {
-        setHasDom(false);
-      }, 500);
+      setIsOpen(route().current.startsWith(props.path));
     }
-  })
+  });
 
-  // Render
+  // Open & close callbacks.
+  // Executes when open state changes but not when allbakcs change
+
+  createEffect(on(isOpen, () => {
+    if (isOpen()) props.onOpen?.();
+    else props.onClose?.();
+  }))
+
+  // DOM & CSS.
+  // Manage correctly timed DOM updates and animation classes
+
+  let container: HTMLElement | undefined;
+  let timeout = setTimeout(() => { }, 0);
+  const [hasDom, setHasDom] = createSignal(false);
+  const [animationClass, setAnimationClass] = createSignal("");
+
+  createEffect(() => {
+    clearTimeout(timeout);
+    const pathLast = props.path.split("/").pop();
+    if (isOpen()) {
+      // set dom regardless
+      setHasDom(true);
+      // animate only the deepest node that changed
+      if (route().firstDifferent[1] === pathLast) setAnimationClass(props.enter ?? "");
+    } else {
+      if (route().firstDifferent[0] === pathLast) {
+        // animate and remove dom of only the deepst node that chagned
+        // there is no need to remove children's dom because they are
+        // supposed to be nested.
+        setAnimationClass(props.leave ?? "");
+        requestAnimationFrame(() => {
+          if (!container) return;
+          const computedStyle = window.getComputedStyle(container);
+          const delay = (
+            Number(computedStyle.animationDelay.slice(0, -1))
+            + Number(computedStyle.animationDuration.slice(0, -1))
+          ) * 1000;
+          timeout = setTimeout(() => {
+            setHasDom(false);
+          }, delay);
+        });
+      }
+    }
+  });
 
   return (
     <Show when={hasDom()}>
-      <section class={classes()}>
+      <section class={`absolute inset-0 ${animationClass()} ${props.class ?? ""}`} ref={container}>
         {props.children}
       </section>
     </Show>
